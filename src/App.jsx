8a887648a776c39b482bae28b0965dace78c871d
Juan Manuel chico <juanmanuelchico@hotmail.com>
2021-08-12 23:44:49 -0300

    Limpiando Proyecto para Crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import './App.css';
-import { Contador } from './components/Contador';
-import  Listado  from './components/Listado';
-import Temperatura from './components/Temperatura';
-import Formulario from './components/Formulario';
 
 function App() {
 
   return (
 
-    <div className="App mt-5">
-      <h1>App in React</h1>
-      <h2>Probando Componentes</h2>
-      <Contador/>
-      <Listado/>
-      <Temperatura/>
-      <Formulario/>
+    <div className="App">
     </div>
 
 );
